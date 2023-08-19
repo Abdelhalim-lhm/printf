e4063ef8e8ec9795755f85f892a485a4bfee8911
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-08-19 15:23:09 -0700

    beta test printf first try

---
+++ printf_header.h
@@ -0,0 +1,24 @@
+#ifndef PRINTF_HEADER_H
+#define PRINTF_HEADER_H
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+int _putchar(char);
+int _printf(const char * const format, ...);
+void print_string(va_list);
+void print_char(va_list);
+void print_percent(va_list);
+/**
+ * struct type - Struct type
+ *
+ * @type: The type of the conversion
+ * @f: The function associated
+ */
+typedef struct type
+{
+	char *type;
+	void (*f)(va_list);
+} type_f;
+#endif
